11c9434ae2d039ae977dc7f403816ec4378543f9
jaydon <06528725jkl>
2016-01-05 14:58:20 +0800

    添加左右滑动

--- app/src/main/java/com/lxf/ruler/view/RulerView.java
+++ app/src/main/java/com/lxf/ruler/view/RulerView.java
@@ -9,10 +9,10 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.widget.Scroller;
 
 import com.lxf.ruler.R;
 import com.lxf.ruler.utils.UiUtils;
@@ -30,7 +30,8 @@ public class RulerView extends View{
     private static final int RULER_MARGIN_TOP_BOTTOM = 10; //距离上下间
     private static final int FIRST_LINE_MARGIN = 5;   // 第一条线距离边框距离
     private static final int DEFAULT_LINE_COUNT = 10; // 打算绘制的厘米数
-    private static final int DEFAULT_LINE_MIDDLE_COUNT = 3;
+    private static final int DEFAULT_LINE_MIDDLE_COUNT = 3; // 一个屏幕放3个
+
     private int mLineHeight; // 最高线的刻度尺高度
     private int mHalfLineHeight; //小线的刻度尺高度
     private int mMiddleLineHeight; // 中间线的刻度尺高度
@@ -45,6 +46,9 @@ public class RulerView extends View{
     private int mRulerHeight = 8;
     private int touchSlop; //最小滑动距离
     private int lineColor;
+    private Scroller mScroller;
+    private int lastX;
+    private int mWholeRulerWidth;
 
     public RulerView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -96,6 +100,7 @@ public class RulerView extends View{
      */
     private void initData() {
         touchSlop = ViewConfiguration.get(mContext).getScaledTouchSlop();
+        this.mScroller = new Scroller(mContext);
         mHalfLineHeight = mLineHeight / 2;
         mMiddleLineHeight = mLineHeight /  3;
         mRulerLeftRightMargin = UiUtils.dipToPx(mContext, RULER_MARGIN_LEFT_RIGHT);
@@ -132,7 +137,7 @@ public class RulerView extends View{
         //第一条线间隔
         canvas.translate((float) (mFirstLineMargin * 1.5 + mRulerLeftRightMargin), 0);
         int top = 0;
-        for(int i = 1; i <= DEFAULT_LINE_COUNT * DEFAULT_LINE_MIDDLE_COUNT; i++) {
+        for(int i = 1; i <= DEFAULT_LINE_COUNT * DEFAULT_LINE_COUNT; i++) {
             //绘制最长线
             if(i % 10 == 0 || i == 1) {
                 top = mTotalHeight - mLineHeight;
@@ -160,7 +165,7 @@ public class RulerView extends View{
         canvas.save();
         //第一条线间隔
         canvas.translate((float) (mFirstLineMargin * 1.5 + mRulerLeftRightMargin) - 2, 0);
-        for(int i = 1; i <= DEFAULT_LINE_COUNT * DEFAULT_LINE_MIDDLE_COUNT; i++) {
+        for(int i = 1; i <= DEFAULT_LINE_COUNT * DEFAULT_LINE_COUNT; i++) {
             canvas.drawText(i + "", 0, mTotalHeight, mTextPaint);
             //增加相应的间隔
             canvas.translate(mLineDivider, 0);
@@ -187,9 +192,11 @@ public class RulerView extends View{
         mTotalWidth = w;
         mTotalHeight = h;
         mLineDivider = (int) Math.round((mTotalWidth - mRulerLeftRightMargin * 2 - mFirstLineMargin * 2) / (DEFAULT_LINE_COUNT * DEFAULT_LINE_MIDDLE_COUNT - 1.0));
-        mOutRect = new Rect(mRulerLeftRightMargin, mRulerTopBottomMargin, mTotalWidth - mRulerLeftRightMargin, mTotalHeight - mRulerTopBottomMargin);
         mRulerSrcRect = new Rect(0, 0, mRulerWidth, mRulerHeight);
         mRulerDestRect = new Rect(0, 0, mRulerWidth, mRulerHeight);
+        mWholeRulerWidth =  (DEFAULT_LINE_COUNT * (DEFAULT_LINE_COUNT - DEFAULT_LINE_MIDDLE_COUNT)) * mLineDivider;
+        mOutRect = new Rect(mRulerLeftRightMargin, mRulerTopBottomMargin, mWholeRulerWidth + mTotalWidth, mTotalHeight - mRulerTopBottomMargin);
+
     }
 
     @Override
@@ -206,11 +213,38 @@ public class RulerView extends View{
                     temp = temp > 0 ? temp : 0;
                     mRulerDestRect = new Rect(temp, 0, temp + mRulerWidth, mRulerHeight);
                     postInvalidate();
+                    scrollerSmoothScrollBy(lastX - x, 0);
                 }
                 break;
             case MotionEvent.ACTION_UP:
                 break;
         }
+        lastX = x;
         return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        //先判断mScroller滚动是否完成
+        if (mScroller.computeScrollOffset()) {
+            //这里调用View的scrollTo()完成实际的滚动
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            //必须调用该方法，否则不一定能看到滚动效果
+            postInvalidate();
+        }
+    }
+
+    //调用此方法滚动到目标位置
+    public void scrollerSmoothScrollTo(int fx, int fy) {
+        int dx = fx - mScroller.getFinalX();
+        int dy = fy - mScroller.getFinalY();
+        scrollerSmoothScrollBy(dx, dy);
+    }
+
+    //调用此方法设置滚动的相对偏移
+    public void scrollerSmoothScrollBy(int dx, int dy) {
+        //设置mScroller的滚动偏移量
+        mScroller.startScroll(mScroller.getFinalX(), mScroller.getFinalY(), dx, dy);
+        invalidate();//这里必须调用invalidate()才能保证computeScroll()会被调用，否则不一定会刷新界面，看不到滚动效果
     }
 }
